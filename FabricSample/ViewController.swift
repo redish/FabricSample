7e7626a0b281b36dd52ec068ac5c3e384e06b2d3
Yusuke Yasuo <yasuo@o-nu.jp>
2016-06-20 21:28:11 +0900

    Initial Commit

---
+++ FabricSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FabricSample
+//
+//  Created by Yusuke Yasuo on 2016/06/20.
+//  Copyright © 2016年 redish.inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
